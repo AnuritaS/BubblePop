646f89423162df527ae60c12cf8d5436e43ad03b
AnuritaS <srivastava.anurita@gmail.com>
2018-11-11 23:48:02 +0530

    Add dismiss keyboard gesture

--- pop-the-bubble/Controller/ViewController.swift
+++ pop-the-bubble/Controller/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var userInputTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
     
     @IBAction func openBubbleVC(_ sender: Any) {
@@ -33,4 +35,7 @@ class ViewController: UIViewController {
         performSegue(withIdentifier: "segueToBubbleVC", sender: self)
     }
 
+    @objc func dismissKeyboard() {
+    view.endEditing(true)
+    }
 }
